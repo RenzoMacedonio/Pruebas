16894177f2cbbd4df22f3fc6b4f39e52462163ba
RenzoMacedonio <renzoutu3b@gmail.com>
2021-12-13 21:39:42 -0300

    Subidas EditTets

--- Assets/Tests/EditMode/EditorTest.cs
+++ Assets/Tests/EditMode/EditorTest.cs
@@ -7,10 +7,41 @@ using UnityEngine.TestTools;
 public class EditorTest
 {
     [Test]
-    public void NoSeMeOcurreNinguna()
+    public void ableToInteract()
     {
-        
+        bool isable = false;
+        if(PuedeInteractuar.interactuable == true){
+            isable = true;
+
+        }
     }
-    
+    [Test]
+    public void SpawnedTurbo()
+    {
+        bool IsSpawned = false;
+        if (GameObject.FindGameObjectWithTag("Turbo") == true)
+        {
+            IsSpawned = true;
+        }
+    }
+    [Test]
+    public void ExistsEnemy()
+    {
+        bool exists = false;
+        if (GameObject.FindGameObjectWithTag("Enemy") == true)
+        {
+            exists = true;
+        }
+    }
+    [Test]
+    public void ExistsPlayer()
+    {
+        bool exists = false;
+        if (GameObject.FindGameObjectWithTag("Player") == true)
+        {
+            exists = true;
+        }
+    }  
    
-}
+}  
+
